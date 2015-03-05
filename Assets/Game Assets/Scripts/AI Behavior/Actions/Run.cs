a30355f9932adef258cc5892925cc74bbca4ccc8
gadgetsan <stephaneblitz@gmail.com>
2015-03-04 23:31:23 -0500

    Was missing some files on last commit

---
+++ Assets/Game Assets/Scripts/AI Behavior/Actions/Run.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Actions
+{
+    class Run : AIAction
+    {
+        public override bool Execute(AIAgent agent)
+        {
+            agent.transform.Translate(new UnityEngine.Vector3(-1, 0, -1));
+            return true;
+        }
+    }
+}
